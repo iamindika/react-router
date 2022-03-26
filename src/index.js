f27c49300419c855cf038993acf532bea875ea6f
iamindika <ikolamba2@gmail.com>
2022-03-26 13:04:14 -0400

    add main index html/javascript

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+  return(
+    <h1>React Router Demo!</h1>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
